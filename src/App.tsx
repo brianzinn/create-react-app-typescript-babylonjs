a71a50b07c06204742b1cb415e96ac8ee920c42a
Brian Zinn <github@wakeskate.com>
2020-03-21 23:20:59 -0700

    Update to BabylonJS 4.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React, { useCallback } from 'react';
 import "@babylonjs/core/Physics/physicsEngineComponent"  // side-effect adds scene.enablePhysics function
-import { Vector3, PhysicsImpostor, Mesh, Nullable } from '@babylonjs/core';
+import { Vector3, PhysicsImpostor, Mesh, Nullable, Color3, FresnelParameters, Texture } from '@babylonjs/core';
 import { CannonJSPlugin } from '@babylonjs/core/Physics/Plugins'
-import { Scene, Engine, withBabylonJS } from 'react-babylonjs';
+import { Scene, Engine } from 'react-babylonjs';
 import './App.css';
 import * as CANNON from 'cannon';
 window.CANNON = CANNON;
 
-const EngineWithContext = withBabylonJS(Engine);
 const gravityVector = new Vector3(0, -9.81, 0);
 let sphere: Nullable<Mesh> = null;
 
@@ -26,7 +25,7 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <p>@babylonjs + `react-babylonjs`</p>
-        <EngineWithContext antialias={true} adaptToDeviceRatio={true} canvasId="sample-canvas">
+        <Engine antialias={true} adaptToDeviceRatio={true} canvasId="sample-canvas">
           <Scene enablePhysics={[gravityVector, new CannonJSPlugin()]}>
             <arcRotateCamera name="arc" target={ new Vector3(0, 1, 0) }
                   alpha={-Math.PI / 2} beta={(0.5 + (Math.PI / 4))}
@@ -35,21 +34,39 @@ const App: React.FC = () => {
             <hemisphericLight name='hemi' direction={new Vector3(0, -1, 0)} intensity={0.8} />
             <directionalLight name="shadow-light" setDirectionToTarget={[Vector3.Zero()]} direction={Vector3.Zero()} position = {new Vector3(-40, 30, -40)}
               intensity={0.4} shadowMinZ={1} shadowMaxZ={2500}>
-              <shadowGenerator mapSize={1024} useBlurExponentialShadowMap={true} blurKernel={32}
+              <shadowGenerator mapSize={1024} useBlurExponentialShadowMap={true} blurKernel={32} darkness={0.8}
                 shadowCasters={["sphere1", "dialog"]} forceBackFacesOnly={true} depthScale={100} />
             </directionalLight>
             <sphere ref={sphereRef} name="sphere1" diameter={2} segments={16} position={new Vector3(0, 2.5, 0)}>
               <physicsImpostor type={PhysicsImpostor.SphereImpostor} _options={{ mass: 1, restitution: 0.9 }} />
-              <plane name="dialog" size={2} position={new Vector3(0, 1.5, 0)}>
-                <advancedDynamicTexture name="dialogTexture" height={1024} width={1024} createForParentMesh={true} hasAlpha={true}>
-                  <rectangle name="rect-1" height={0.5} width={1} thickness={12} cornerRadius={12}>
-                      <rectangle>
-                        <babylon-button name="close-icon" background="green" onPointerDownObservable={onButtonClicked} >
-                          <textBlock text={'\uf00d click me'} fontFamily="FontAwesome" fontStyle="bold" fontSize={200} color="white" />
-                        </babylon-button>
-                      </rectangle>
-                  </rectangle>
-                </advancedDynamicTexture>
+              <standardMaterial name='material1' specularPower={16}
+                diffuseColor={Color3.Black()}
+                emissiveColor={new Color3(0.5, 0.5, 0.5)}
+                reflectionFresnelParameters={FresnelParameters.Parse({
+                    isEnabled: true,
+                    leftColor: [1, 1, 1],
+                    rightColor: [0, 0, 0],
+                    bias: 0.1,
+                    power: 1
+                })}
+              />
+              <plane name="dialog" size={2} position={new Vector3(0, 1.5, 0)} sideOrientation={Mesh.BACKSIDE}>
+                <advancedDynamicTexture
+                    name="dialogTexture"
+                    height={1024} width={1024}
+                    createForParentMesh={true}
+                    hasAlpha={true}
+                    generateMipMaps={true}
+                    samplingMode={Texture.TRILINEAR_SAMPLINGMODE}
+                  >
+                    <rectangle name="rect-1" height={0.5} width={1} thickness={12} cornerRadius={12}>
+                        <rectangle>
+                            <babylon-button name="close-icon" background="green" onPointerDownObservable={onButtonClicked} >
+                            <textBlock text={'\uf00d click me'} fontFamily="FontAwesome" fontStyle="bold" fontSize={200} color="white" />
+                            </babylon-button>
+                        </rectangle>
+                    </rectangle>
+                  </advancedDynamicTexture>
               </plane>
             </sphere>
             
@@ -58,7 +75,7 @@ const App: React.FC = () => {
             </ground>
             <vrExperienceHelper webVROptions={{ createDeviceOrientationCamera: false }} enableInteractions={true} />
           </Scene>
-        </EngineWithContext>
+        </Engine>
       </header>
     </div>
   );
